886c5bb53ae3f92d8f7918428bce1a8ff554a5eb
KaatneWalaKutta <46840759+KaatneWalaKutta@users.noreply.github.com>
2026-02-20 03:42:16 +0530

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,11 @@
-start cmd.exe
-start control.exe
+@echo off
+echo ========================================= > %TEMP%\boom.txt
+echo PENTEST VDI BREAKOUT SUCCESSFUL >> %TEMP%\boom.txt
+echo ========================================= >> %TEMP%\boom.txt
+echo. >> %TEMP%\boom.txt
+echo USERNAME: >> %TEMP%\boom.txt
+whoami >> %TEMP%\boom.txt
+echo. >> %TEMP%\boom.txt
+echo RUNNING PROCESSES: >> %TEMP%\boom.txt
+tasklist >> %TEMP%\boom.txt
+start notepad %TEMP%\boom.txt
